3c629480f29cda080eff7b8d8bea44fabc394514
John <jonnyuz99@gmail.com>
2019-05-03 20:15:13 +0300

    pictures and letters

---
+++ Snake/main.c
@@ -0,0 +1,115 @@
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_exti.h"
+#include "stm32f0xx_ll_utils.h"
+#include "stm32f0xx_ll_cortex.h"
+#include "stm32f0xx_ll_tim.h"
+
+
+#define CHECK_DELAY 150
+#define FREQUENCY_48KHZ 47999
+#define FIRST_PRIOR 1
+
+#define CONVERT_TO_SEC(ARG) (ARG) = (ARG) / 1000
+
+static int counter = 0; // a counter for dinamic blinking
+static double time = 0; // a period between pressing
+
+void SetDigit(int digit);
+void PrintNumber(int counter, double number);
+
+void gpio_config();
+void rcc_config();
+void exti_config();
+
+static void timers_config(void)
+{
+    //Configure input channel
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+    LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_5, LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetAFPin_0_7(GPIOA, LL_GPIO_PIN_5, LL_GPIO_AF_2);
+    LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_5, LL_GPIO_PULL_UP);
+
+    /*
+     * Setup timer to capture input mode
+     */
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+    LL_TIM_SetPrescaler(TIM2, FREQUENCY_48KHZ);
+    LL_TIM_IC_SetFilter(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_IC_FILTER_FDIV32_N8);
+    LL_TIM_IC_SetPolarity(TIM2, LL_TIM_CHANNEL_CH1,
+                          LL_TIM_IC_POLARITY_RISING);
+
+    LL_TIM_IC_SetActiveInput(TIM2, LL_TIM_CHANNEL_CH1,
+                             LL_TIM_ACTIVEINPUT_DIRECTTI);
+
+    LL_TIM_IC_SetPrescaler(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_ICPSC_DIV1);
+    LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
+    LL_TIM_EnableIT_CC1(TIM2);
+    LL_TIM_EnableCounter(TIM2);
+    /*
+     * Setup NVIC
+     */
+    NVIC_EnableIRQ(TIM2_IRQn);
+    NVIC_SetPriority(TIM2_IRQn, FIRST_PRIOR);
+
+    LL_TIM_GenerateEvent_UPDATE(TIM2);
+}
+
+void TIM2_IRQHandler(void)
+{
+    static double prev = 0; // a moment of a previous pressing
+    static double next = 0; // a moment of a present moment
+
+    prev = next;
+    next = LL_TIM_IC_GetCaptureCH1(TIM2);
+    if((next - prev) > CHECK_DELAY)
+    {
+        time = (next - prev);
+        CONVERT_TO_SEC(time);
+        LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+        LL_TIM_ClearFlag_CC1(TIM2);
+    }
+}
+
+static void systick_config(void)
+{
+    LL_InitTick(48000000, 1000);
+    LL_SYSTICK_EnableIT();
+    NVIC_SetPriority(SysTick_IRQn, 1);
+
+    return;
+}
+
+void SysTick_Handler(void)
+{
+    static int temp = 0;
+    temp = (temp + 1) % 5;
+    counter = (counter + 1) % 4;
+
+    if(temp)
+    {
+        PrintNumber(counter, time);
+        LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_9);
+    }
+}
+
+void EXTI0_1_IRQHandler(void)
+{
+    LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+
+    LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_1);
+}
+
+int main(void)
+{
+    rcc_config();
+    gpio_config();
+    timers_config();
+    systick_config();
+    exti_config();
+
+    while (1);
+    return 0;
+}
---
+++ Snake/system.c
@@ -0,0 +1,82 @@
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_exti.h"
+#include "stm32f0xx_ll_utils.h"
+#include "stm32f0xx_ll_cortex.h"
+
+void gpio_config(void)
+{
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+    
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+    LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_1, LL_GPIO_PULL_DOWN);
+    LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_0, LL_GPIO_PULL_DOWN);
+
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+    
+    LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_INPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
+}
+
+void rcc_config()
+{
+    /* Set FLASH latency */
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+    /* Enable HSI and wait for activation*/
+    LL_RCC_HSI_Enable();
+
+    while (LL_RCC_HSI_IsReady() != 1);
+
+    /* Main PLL configuration and activation */
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+                                LL_RCC_PLL_MUL_12);
+
+    LL_RCC_PLL_Enable();
+    while (LL_RCC_PLL_IsReady() != 1);
+
+    /* Sysclk activation on the main PLL */
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+
+    /* Set APB1 prescaler */
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+    /* Update CMSIS variable (which can be updated also
+     * through SystemCoreClockUpdate function) */
+    SystemCoreClock = 48000000;
+}
+
+void exti_config(void)
+{
+    LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
+
+    LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE1);
+
+    LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_1);
+
+    LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_1);
+    LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_1);
+    
+    NVIC_EnableIRQ(EXTI0_1_IRQn);
+    NVIC_SetPriority(EXTI0_1_IRQn, 0);
+}
+
